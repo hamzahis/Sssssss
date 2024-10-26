41a2a5d860354bd3d99ea6b9fba428b06e8aa0cf
hamzahis <164903490+hamzahis@users.noreply.github.com>
2024-10-26 21:01:03 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,14 +1,18 @@
 from flask import Flask, request, make_response, render_template
+import webbrowser
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def get_public_ip():
-    # The 'X-Forwarded-For' header contains the public IP of the requester
-    public_ip = request.headers.get('Cookie')
-    return f"Your public IP is: {public_ip}"
+def home():
+    url = "https://www.example.com"
+    webbrowser.open(url)
 
-
-
+  #  public_ip = request.headers.get('Cookie')
+#    return f"Your public IP is: {public_ip}"
+  #  response = make_response('Hello, world!')
+  #  response.set_cookie('yummy_cookie',   'chocolate')
+  #  return response
 
