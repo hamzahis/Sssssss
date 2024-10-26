231023d53fc605252552f97de76331c1a2b4a916
hamzahis <164903490+hamzahis@users.noreply.github.com>
2024-10-26 22:35:20 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,6 +7,7 @@ def index():
   url = "https://casablanca-s78l.vercel.app/"
   link_html = f'<a href="{url}">Visit Example Site</a>'
   return link_html
+  
   response = make_response('Hello, world!')
   response.set_cookie('hamza','male')
   return response
