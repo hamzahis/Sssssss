200843fe0cee4e7a782b51d6d0df4fe66530af79
hamzahis <164903490+hamzahis@users.noreply.github.com>
2024-10-26 22:36:43 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  url = "https://casablanca-s78l.vercel.app/"
-  link_html = f'<a href="{url}">Visit Example Site</a>'
-  return link_html
+#  url = "https://casablanca-s78l.vercel.app/"
+#  link_html = f'<a href="{url}">Visit Example Site</a>'
+#  return link_html
   
   response = make_response('Hello, world!')
   response.set_cookie('hamza','male')
