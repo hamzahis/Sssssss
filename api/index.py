a9141b1a1a1ff4edde695c8f2f8a0d93ad68d4f2
hamzahis <164903490+hamzahis@users.noreply.github.com>
2024-10-27 12:29:41 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,12 +3,17 @@ from flask import Flask, request, make_response, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-  url = "https://casablanca-s78l.vercel.app/"
-  link_html = f'<a href="{url}">Visit Example Site</a>'
-  response = make_response('Hello, world!')
-  response.set_cookie('hamza','male')
-  return response, link_html
+def home():
+    url = "https://casablanca-s78l.vercel.app/"
+    link_html = f'<a href="{url}">Visit Example Site</a>'
+
+    # Create the response with the link HTML
+    response = make_response(link_html)
+    
+    # Set the cookie
+    response.set_cookie('hamza', 'one')
+    
+    return response
 
 
   #  public_ip = request.headers.get('Cookie')
