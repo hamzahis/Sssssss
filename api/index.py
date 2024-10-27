bf955208f8f47958f0facc0de5be01181715c02e
hamzahis <164903490+hamzahis@users.noreply.github.com>
2024-10-27 15:19:05 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,41 @@
+from flask import Flask, make_response, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def set_cookie():
+    url = "https://casablanca-s78l.vercel.app/"
+    link_html = f'<a href="{url}">Visit Example Site</a>'
+    
+    # Create the response with the link HTML
+    response = make_response(link_html)
+    
+    # Set a cookie that can be accessed by the third-party
+    response.set_cookie('user_data', 'hamza', samesite='None', secure=True)
+    
+    return response
+
+@app.route('/get-cookie')
+def get_cookie():
+    # This endpoint allows a third-party site to access the cookie value
+    user_data = request.cookies.get('user_data')
+    if user_data:
+        return jsonify({"user_data": user_data})
+    else:
+        return jsonify({"error": "No user data found"}), 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+"""
 from flask import Flask, render_template, make_response, request
 
 app = Flask(__name__)
@@ -13,7 +51,7 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
+"""
 
 
 
