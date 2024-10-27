8b586d7f168a954f528e6df43b0dc21ab1ebb0bb
hamzahis <164903490+hamzahis@users.noreply.github.com>
2024-10-27 14:03:57 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,30 @@
+from flask import Flask, render_template, make_response, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    if 'first_visit' not in request.cookies:
+        resp = make_response(render_template('index.html'))
+        resp.set_cookie('first_visit', 'true')  # 30 days
+        return resp
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+"""
+
 from flask import Flask, request, make_response, render_template
 
 app = Flask(__name__)
@@ -14,7 +41,7 @@ def home():
     response.set_cookie('hamza', 'one', samesite='None', secure=True)
     
     return response
-
+"""
 
   #  public_ip = request.headers.get('Cookie')
 #    return f"Your public IP is: {public_ip}"
